4245f3a1393f36a57f8aa57801128086b8161521
Evgeniy Titov <falgore88@gmail.com>
2023-09-30 19:45:58 +0300

    fix

--- app.py
+++ app.py
@@ -1,13 +1,29 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+import csv
+import random
 
 app = Flask(__name__)
 
+data = []
+with open('test.csv', 'r') as f:
+    reader = csv.reader(f)
+    for row in reader:
+        data.append({
+            'category': row[0],
+            'theme': row[1]
+        })
+
 
 def handler():
-    return jsonify({
-        'category': 'test',
-        'theme': 'test'
-    })
+    if request.args.get('url'):
+        url = request.args.get('url')
+    elif request.args.get('domain'):
+        url = request.args.get('domain')
+    elif request.is_json:
+        url = request.json.get('url')
+    
+    print(url)
+    return jsonify(random.choice(data))
 
 
 @app.route('/check_domain', methods=['GET', 'POST'])
